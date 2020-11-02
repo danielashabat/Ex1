0ab1c2b12a7ea982bebcc0b329fb12beebe9677f
danielashabat <danielashabat@gmail.com>
2020-11-02 21:18:18 +0200

    fix 2d array to string

--- Ex1/Father/Father.h
+++ Ex1/Father/Father.h
@@ -6,12 +6,11 @@
 	exit(1);\
 }
 void father(FILE * input_file, FILE * output_file);
-char** load_next_generation(char** forest_table, unsigned int dimensions);
-char** allocate_memory_to_forest_table(unsigned int dimensions);
-void free_forest_table(char** forest_table, unsigned int dimensions);
-void read_forest_table(FILE * input_file, char** forest_table, unsigned int dimensions);
-void change_fire_to_ground(char** forest_table, char** new_forest_table, unsigned int dimensions);
-void change_ground_to_tree(char** forest_table, char** new_forest_table, unsigned int dimensions);
-void spread_fire(char** forest_table, char** new_forest_table, unsigned int dimensions);
+char* load_next_generation(char* forest_table, unsigned int dimensions);
+char* allocate_memory_to_forest_string(unsigned int dimensions);
+void read_forest_table(FILE * input_file, char* forest_table, unsigned int d);
+static void change_fire_to_ground(char* forest_table, char* new_forest_table, unsigned int d);
+void change_ground_to_tree(char* forest_table, char* new_forest_table, unsigned int d);
+void spread_fire(char* forest_table, char* new_forest_table, unsigned int d);
 #endif
 #pragma once
