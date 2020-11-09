23e2ad1b21b71cf4db1a760be7bfe48bca01aaab
anat sinai <anatsinai1@gmail.com>
2020-11-09 20:34:18 +0200

    add tid  and son comments

--- Ex1/Son/main.c
+++ Ex1/Son/main.c
@@ -1,10 +1,22 @@
+/*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
+/*
+ Authors: Anat Sinai 312578149 and Daniela Shabat 316415645
+ This project was written for Tel Aviv unviersity course for learning about creating processes
+ Project name: 'Exercise 1'
+ decsription: this module runs the son's main of the project
+*/
+/*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+int calculate_number_of_burned_trees(char* forest_table);
+/*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
 
 int main(int argc, char* argv[]) {
 	
 	int num_of_burned_trees = 0;
+	//checking that we got enough argument  
 	if (argc < 2) {
 		printf("ERROR:not enough arguments!");
 		return 1;
@@ -14,6 +26,7 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	else {
+		//getting foredt table string from the father program  
 		char* forest_string = argv[1];
 		num_of_burned_trees = calculate_number_of_burned_trees(forest_string);
 	}
